d4f93c09036eccb9960337084518e5070a7ed47a
xeulbn <gsbtiger0215@gmail.com>
2024-03-28 22:46:57 +0900

    빡구현 끝

--- src/main/java/lotto/Result.java
+++ src/main/java/lotto/Result.java
@@ -8,21 +8,18 @@ public class Result {
 
     static int[] result=new int[5];
 
+    static double plus;
+
     static void printResult(){
         System.out.println("3개 일치 (5000원) - "+result[0]);
         System.out.println("4개 일치 (50,000원) - "+result[1]);
         System.out.println("5개 일치 (1,500,000원) - "+result[2]);
         System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - "+result[3]);
         System.out.println("6개 일치 (2,000,000,000원) - "+result[4]);
-        System.out.println("총 수익률은 "+" 입니다.");
+        System.out.println("총 수익률은 "+plus+"%입니다.");
     }
 
     static void letMatch(){
-        result[0]=0;
-        result[1]=0;
-        result[2]=0;
-        result[3]=0;
-        result[4]=0;
 
         for(int i=0;i<(lottoPrice/1000);i++){
             if(matchArray[i]==3){
@@ -38,4 +35,12 @@ public class Result {
             }
         }
     }
+
+    static double investmentRate(){
+        int total;
+        total=result[0]*5000+result[1]*50000+result[2]*1500000+result[3]*30000000+result[4]*2000000000;
+        plus=(total/lottoPrice);
+        return plus;
+    }
+
 }
